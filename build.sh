21868251ecfd5f0fdb6a93e9122cd2ef5c15e757
Isabella Basso <isabellabdoamaral@gmail.com>
2026-05-27 19:44:49 -0300

    build: replace git clone --revision with checkout fallback

--- build.sh
+++ build.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env bash
 
-[ -d clay ] || git clone --revision b16c7a48e1274e06b4a8826fc0f21b093f0a9396 https://github.com/catermujo/clay --depth 1
+clone_at_revision() {
+    local dir="$1"
+    local revision="$2"
+    local remote="$3"
+    shift 3
+    [ -d "$dir" ] && return
+    git clone "$@" "$remote" "$dir"
+    if ! git -C "$dir" checkout --detach "$revision"; then
+        git -C "$dir" fetch origin "$revision"
+        git -C "$dir" checkout --detach FETCH_HEAD
+    fi
+    if [ -f "$dir/.gitmodules" ]; then
+        git -C "$dir" submodule update --init --recursive
+    fi
+}
+
+clone_at_revision clay b16c7a48e1274e06b4a8826fc0f21b093f0a9396 https://github.com/catermujo/clay --depth=1
 # git -C clay pull --rebase
 
 ./build-clay-lib.sh
